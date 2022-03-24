ffe4ae09cb92612daa361c22c1658e5a634a3716
leemr <think-roon@naver.com>
2022-03-24 10:51:13 +0900

    logback 설정

---
+++ src/main/java/com/skp/intern/interceptor/LogInterceptor.java
@@ -0,0 +1,24 @@
+package com.skp.intern.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LogInterceptor implements HandlerInterceptor {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.debug("{} called",handler.toString());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        logger.debug("{} 가 종료됨. {} 를 view로 사용",handler.toString(),modelAndView.getViewName());
+    }
+}
